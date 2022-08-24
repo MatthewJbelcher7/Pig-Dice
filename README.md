29999160d12ee53e1da0e0807eb70de1782f1b8b
Matthew Belcher <matthewjbelcher7@live.com>
2022-08-24 16:40:53 -0600

    WIP
    Co-authored-by: Grace Lee <gracexsnoopy@gmail.com>

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 Describe numberRandomizer()
 
-Test: "Create a function that randomize's 1-6."
+<!-- Test: "Create a function that randomize's 1-6."
 Code: getRandomNumber();
-Expected Ouput: random number
+Expected Ouput: random number -->
 
 Test: "Create a function that randomize's 1-6 and stores outcome."
 Code:
@@ -12,22 +12,54 @@ Test: "Create a function that randomize's 1-6 and stores outcome and add's follo
 Code:
 Expected Ouput:
 
+Test: Create true if roll = 1 
+Code:
+Expected Ouput: If rolls 1 then True and current score = 0 (so nothing gets added to total score)
+
+Test: If True switch players 
+Code:
+Expected Ouput: If roll is true switch active player.
+
+Test: If False add score to current score
+Code:
+Expected Ouput: Current score + 2-6 roll
+
+Test: If active player presses hold button (use onclick event)
+Code:
+Expected Output: add current score to total score
+
+Test: If active player presses hold button (use onclick event), once current score is added to total score
+Code:
+Exptected Output: it would switch active player
+
+Test: Check if score is >= 100
+Code:
+Expected Ouput:
+
+Test: if total score >= 100 (true)
+Code:
+Expected Ouput: determine the winner (prompt the winner)
+
+Test: Create a function that resets the game 
+Code:
+Expected Ouput: The current and total scores of both players = 0
+
 Test: "Create a button that prompts the user to roll"
 Code:
 Expected Ouput:
 
-Test: 
+Test: Create Player 1 and 2 object 
 Code:
 Expected Ouput:
 
-Test: 
+Test: Create Score 1 and 2 object
 Code:
 Expected Ouput:
 
-Test: 
+Test: Create a baseline starting score of 0
 Code:
 Expected Ouput:
 
-Test: 
+Test: Create current score and total score (separate for each players)
 Code:
 Expected Ouput:
--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,61 @@
 function getRandomNumber(min, max) {
   min = Math.ceil(1);
-  max = Math.floor(6);
+  max = Math.floor(7);
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
 
-scoreOneArray.push(parseInt(getRandomNumber()));
-scoreTwoArray.push(parseInt(getRandomNumber()));
-const scoreOneArray = [];
-const scoreTwoArray = [];
-
-function score1() {
-  let sum = 0 
-  for (i = 0; i < scoreOneArray.length; i++) {
-    sum += scoreOneArray[i]
-  }
-  return sum;
-};
-
-function score2() {
-  let sum = 0 
-  for (i = 0; i < scoreTwoArray.length; i++) {
-    sum += scoreTwoArray[i]
-  }
-  return sum;
-};
-
-function Player1() {
-  this.score = score1();
-
+function Player(currentScore, totalScore) {
+  this.currentScore = currentScore;
+  this.totalScore = totalScore;
 }
 
+function Player2(currentScore, totalScore) {
+  this.currentScore = currentScore;
+  this.totalScore = totalScore;
+}
 
-document.getElementById("roll-dice").onclick = getRandomNumber();
+function score(diceValue) {
+  var currentScoreSum = 0; 
+  let currentScoreArray = [];
+  for (let i= 0; i < diceValue.length; i++) {
+    currentScoreArray.push([i]);
+  }
+  for (let i = 0; i < currentScoreArray.length; i++) {
+      currentScoreSum += currentScoreArray[i]
+  } 
+}
+
+let diceValue = getRandomNumber();
+
+// scoreOneArray.push(parseInt(getRandomNumber()));
+// scoreTwoArray.push(parseInt(getRandomNumber()));
+// const scoreOneArray = [];
+// const scoreTwoArray = [];
+
+// function score1() {
+// 
+//   for (i = 0; i < currentScoreArray.length; i++) {
+//     currentScoreSum += currentScoreArray[i]
+//   }
+//   return sum;
+// };
+
+// function score2() {
+//   let sum = 0 
+//   for (i = 0; i < scoreTwoArray.length; i++) {
+//     sum += scoreTwoArray[i]
+//   }
+//   return sum;
+// };
+
+// function Player1() {
+//   this.score = score1();
+
+// }
+const rollButton = document.getElementById("roll-dice");
+rollButton.addEventListener("click", getRandomNumber);
+
+function pushRandomNumber() {
+  document.getElementById("roll-dice").onclick = getRandomNumber();
+  return document.getElementsByClassName(".player-one-score").innerText = randomNumber;
+} 
